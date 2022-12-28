41f74b2820ccc98496abee3eb0e01757a69e3781
Ashik Kabeer <ashikkabeer17@gmail.com>
2022-12-28 16:30:04 +0530

    first commit

---
+++ public/script.js
@@ -0,0 +1,69 @@
+window.onload = function() {
+    const converter = new showdown.Converter();
+    const pad = document.getElementById('pad');
+    const markdownArea = document.getElementById('markdown');
+
+    // make the tab act like a tab
+    pad.addEventListener('keydown',function(e) {
+        if(e.keyCode === 9) { // tab was pressed
+            // get caret position/selection
+            const start = this.selectionStart;
+            const end = this.selectionEnd;
+
+            const target = e.target;
+            const value = target.value;
+
+            // set textarea value to: text before caret + tab + text after caret
+            target.value = value.substring(0, start)
+                            + "\t"
+                            + value.substring(end);
+
+            // put caret at right position again (add one for the tab)
+            this.selectionStart = this.selectionEnd = start + 1;
+
+            // prevent the focus lose
+            e.preventDefault();
+        }
+    });
+
+    var previousMarkdownValue;
+
+    // convert text area to markdown html
+    const convertTextAreaToMarkdown = function(){
+        var markdownText = pad.value;
+        previousMarkdownValue = markdownText;
+        html = converter.makeHtml(markdownText);
+        markdownArea.innerHTML = html;
+    };
+
+    const didChangeOccur = function(){
+        if(previousMarkdownValue != pad.value){
+            return true;
+        }
+        return false;
+    };
+
+    // check every second if the text area has changed
+    setInterval(function(){
+        if(didChangeOccur()){
+            convertTextAreaToMarkdown();
+        }
+    }, 1000);
+
+    // convert textarea on input change
+    pad.addEventListener('input', convertTextAreaToMarkdown);
+
+    // ignore if on home page
+    if(document.location.pathname.length > 1){
+        // implement share js
+        const documentName = document.location.pathname.substring(1);
+        sharejs.open(documentName, 'text', function(error, doc) {
+            doc.attach_textarea(pad);
+            convertTextAreaToMarkdown();
+        });
+    }
+
+    // convert on page load
+    convertTextAreaToMarkdown();
+
+};
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const sharejs = require('share')
+require('redis')
+app.set('view engine', 'ejs');
+
+   
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+res.render('pad');
+});
+app.get('/(:id)',function(req,res) { 
+    res.render('pad')
+})
+
+const options = {
+    db: {type: 'redis'}
+}
+
+sharejs.server.attach(app,options);
+
+const port = process.env.PORT || 8000;
+app.listen(port);
